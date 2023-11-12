adc691a1e57cfee2c639b9718f11e00c50331939
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-12 16:12:00 +0200

    Add z-score calculation to fit method and use it in transform method to remove outliers

--- notebooks/transformers/outlier.py
+++ notebooks/transformers/outlier.py
@@ -6,8 +6,15 @@ import numpy as np
 class OutlierTransformer(BaseEstimator, TransformerMixin):
     def __init__(self, threshold=3):
         self.threshold = threshold
+        self.training_z_scores = None
 
     def fit(self, X, y=None):
+        """Calculate the z-score for each column in the DataFrame"""
+        X_copy = X.copy().to_numpy()
+        column = X_copy[:, 0]
+        z_scores = self.__zscore(column)
+
+        self.training_z_scores = z_scores
         return self
 
     def __zscore(self, X):
@@ -16,13 +23,11 @@ class OutlierTransformer(BaseEstimator, TransformerMixin):
 
     def transform(self, X: pd.DataFrame, y=None):
         """
-        Calculate the z-score for each column in the DataFrame and remove the rows with z-score > threshold
+        remove the rows with z-score > threshold
         """
-
         X_copy = X.copy().to_numpy()
-        column = X_copy[:, 0]
-        z_scores = self.__zscore(column)
-        outliers = np.abs(z_scores) > self.threshold
+
+        outliers = np.abs(self.training_z_scores) > self.threshold
         X_copy[outliers] = np.nan
 
         return pd.DataFrame(X_copy, columns=X.columns, index=X.index)
