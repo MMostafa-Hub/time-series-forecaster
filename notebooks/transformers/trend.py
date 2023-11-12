9d4a66179e47cccdaef67a36c93e4cefad0d8bd9
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-12 16:11:16 +0200

    Refactored trend transformer to use expanding mean instead of linear regression

--- notebooks/transformers/trend.py
+++ notebooks/transformers/trend.py
@@ -5,38 +5,26 @@ import pandas as pd
 import numpy as np
 
 
-# Create Trend Transformer class
 class TrendTransformer(BaseEstimator, TransformerMixin):
-    def __init__(self, window_size=10, degree=3):
+    def __init__(self, window_size=10):
         self.window_size = window_size
-        self.degree = degree
-        self.lr = LinearRegression()
-        self.poly = PolynomialFeatures(degree=self.degree)
 
     def fit(self, X: pd.DataFrame, y=None):
-        X_copy = X.copy()
-
-        values = X_copy["value"]
-        timestamps = X_copy.index.values.reshape(-1, 1).astype(float)
-
-        # Calculate the trend using the window average
-        expanding_mean = values.expanding(self.window_size).mean().bfill().to_numpy()
-
-        # Create the features for the linear regression
-        features = self.poly.fit_transform(timestamps)
-
-        # Fit the linear regression
-        self.lr.fit(features, expanding_mean.reshape(-1, 1))
-
         return self
 
     def transform(self, X: np.ndarray, y=None) -> pd.DataFrame:
         X_copy = X.copy()
-        timestamps = X_copy.index.values.reshape(-1, 1).astype(float)
+        values = X_copy["value"]
 
-        features = self.poly.fit_transform(timestamps)
-        trend = self.lr.predict(features)
+        # Calculate the trend using the window average
+        expanding_mean = values.expanding(self.window_size).mean().bfill().to_numpy()
 
         return pd.concat(
-            [X, pd.DataFrame(data=trend, index=X.index, columns=["trend"])], axis=1
+            [
+                X,
+                pd.DataFrame(
+                    data=expanding_mean, index=X_copy.index, columns=["trend"]
+                ),
+            ],
+            axis=1,
         )
