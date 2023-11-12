3df7e003c56b102c712c5a1fbac2de6f4906122d
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-12 23:53:39 +0200

    Add API endpoint for time series forecasting

--- time_series_forecaster/api/views.py
+++ time_series_forecaster/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class ForecastView(APIView):
+    def get(self, request) -> Response:
+        return Response({"prediction": 0.5}, status=status.HTTP_200_OK)
