982dd48c130c0c26c427f307617f1cca9a39508c
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-13 09:32:28 +0200

    Add Dataset and Forecast models to API

--- time_series_forecaster/api/models.py
+++ time_series_forecaster/api/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Dataset(models.Model):
+    dataset_id = models.CharField(max_length=255, unique=True, primary_key=True)
+    model = models.BinaryField()
+
+    def __str__(self):
+        return f"Dataset ID: {self.dataset_id}"
+
+    def get_model(self):
+        import pickle
+
+        return pickle.loads(self.model)
+
+    def set_model(self, model):
+        import pickle
+
+        self.model = pickle.dumps(model)
+
+
+class Forecast(models.Model):
+    dataset_id = models.CharField(max_length=255, foreign_key=True)
+    values = models.JSONField()
+    prediction = models.FloatField()
+
+    def __str__(self):
+        return f"Dataset ID: {self.dataset_id}, Values: {self.values}, Prediction: {self.prediction}"
