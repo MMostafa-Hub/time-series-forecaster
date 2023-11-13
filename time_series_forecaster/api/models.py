add3462bfe24124ed092c9598d26049503357738
MMostafa-Hub <moh.mostafa.ibra@gmail.com>
2023-11-13 11:20:43 +0200

    Create serializers for and add validation

--- time_series_forecaster/api/models.py
+++ time_series_forecaster/api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Dataset(models.Model):
-    dataset_id = models.CharField(max_length=255, unique=True, primary_key=True)
-    model = models.BinaryField()
+    dataset_id = models.CharField(max_length=255, unique=True, primary_key=True, name="dataset_id")
+    model = models.BinaryField(name="model")
 
     def __str__(self):
         return f"Dataset ID: {self.dataset_id}"
@@ -15,9 +15,9 @@ class Dataset(models.Model):
 
 
 class Forecast(models.Model):
-    dataset = models.ForeignKey(Dataset, on_delete=models.CASCADE)
-    values = models.JSONField()
-    prediction = models.FloatField()
+    dataset_id = models.ForeignKey(Dataset, on_delete=models.CASCADE)
+    values = models.JSONField(name="values")
+    prediction = models.FloatField(null=True, name="prediction")
 
     def __str__(self):
         return f"Dataset ID: {self.dataset_id}, Values: {self.values}, Prediction: {self.prediction}"
--- time_series_forecaster/api/views.py
+++ time_series_forecaster/api/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
-
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.http import JsonResponse
+from .models import Forecast, Dataset
+from .serializers import ForecastSerializer
 
 class ForecastView(APIView):
     def get(self, request) -> Response:
-        return Response({"prediction": 0.5}, status=status.HTTP_200_OK)
+        ForecastSerializer(data=request.data).validate(request.data)
+    
+        return Response(status=status.HTTP_200_OK)
