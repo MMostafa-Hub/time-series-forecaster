c5eae65f72b53729d01c0f3303215a9e626a14d2
MMostafa-Hub <mmostafa.ibra21@gmail.com>
2023-12-02 14:17:41 +0200

    Refactor code to handle test dataset and start timestamp in ForecastView

--- time_series_forecaster/api/views.py
+++ time_series_forecaster/api/views.py
@@ -12,7 +12,12 @@ class ForecastView(APIView):
     def get(self, request) -> Response:
         request_body = request.data
 
-        dataset_id = request_body["dataset_id"]
+        # Get the request parameters
+        dataset_id = "train_" + request_body["dataset_id"]
+        start_timestamp = pd.to_datetime(request_body["start_timestamp"])
+        test_dataset_path = request_body["test_dataset_path"]
+        test_dataset = pd.read_csv(test_dataset_path, index_col=0, parse_dates=True)
+
         client = MLFlowExperiment(dataset_id)
 
         # Load the preprocessing pipeline and the model
@@ -25,17 +30,10 @@ class ForecastView(APIView):
         interval = pd.Timedelta(params["index_interval"])
         max_lags = int(params["max_lags"])
 
-        # Check if the request body has the required values
-        if len(request_body["values"]) < max_lags:
-            return JsonResponse(
-                {
-                    "error": f"Please provide at least {max_lags} values to make a prediction."
-                },
-                status=400,
-            )
-
         # Transform the input to a dataframe
-        transformed_input = self.__get_test_input(request_body["values"], interval)
+        transformed_input = self.__get_test_input(
+            test_dataset, start_timestamp, max_lags, interval
+        )
 
         # Transform the input to the format expected by the model
         pre_transformed_input = pipeline.transform(transformed_input)
@@ -55,25 +53,28 @@ class ForecastView(APIView):
         return JsonResponse(response, status=status.HTTP_200_OK)
 
     def __get_test_input(
-        self, values: List[dict], interval: pd.Timedelta
+        self,
+        test_dataset: pd.DataFrame,
+        start_timestamp: pd.DatetimeIndex,
+        max_lags: int,
+        interval: pd.Timedelta,
     ) -> pd.DataFrame:
-        transformed_input = self.__input_to_dataframe(values)
+        # Search for the start_timestamp in the test dataset
+        if start_timestamp not in test_dataset.index:
+            raise ValueError("The start_timestamp is not in the test dataset.")
+        
+        test = test_dataset.loc[start_timestamp:]
 
+        # Take the first max_lags rows
+        if len(test) < max_lags:
+            raise ValueError("Not enough data to make a prediction.")
+
+        transformed_input = test.iloc[:max_lags, :]
+        
+        
         # add a new record with the last time index + interval
         last_time_index = transformed_input.index[-1]
         new_time_index = last_time_index + interval
-
+        
         transformed_input.loc[new_time_index] = np.nan
-
         return transformed_input
-
-    def __input_to_dataframe(self, values: List[dict]) -> pd.DataFrame:
-        time_index_list = []
-        value_list = []
-        for value in values:
-            value_list.append(value["value"])
-            time_index_list.append(value["time"])
-
-        # Change the type of the time index to datetime
-        time_index_list = pd.to_datetime(time_index_list)
-        return pd.DataFrame({"value": value_list}, index=time_index_list)
